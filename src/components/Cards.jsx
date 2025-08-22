8508b924ce98d99112a9bf05a9af6a90a5dd9347
gnehemias04 <gnehemias04@gmail.com>
2025-08-22 10:33:39 -0300

    cambios de cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -36,11 +36,11 @@ export default function Cards() {
                   alt={meal.strMeal}
                   className="bg-cover rounded-2xl"
                 />
-                <h2 className="text-2xl text-white absolute text-end bottom-4 m-4">
+                <h2 className="text-2xl text-white p-2 px-4 bg-black/60 rounded-2xl absolute text-end bottom-4 m-4">
                   {meal.strMeal}
                 </h2>
               </div>
-              <div className="absolute inset-0 rounded-2xl opacity-0 hover:opacity-100 trancition-all duration-500 bg-[#83ba67] flex flex-col justify-center items-center text-white font-bold gap-10">
+              <div className="absolute inset-0 rounded-2xl opacity-0 hover:opacity-100 trancition-all duration-500 bg-[#83ba67]/85 flex flex-col justify-center items-center text-white font-bold gap-10">
                 <h2 className="text-2xl ">{meal.strMeal}</h2>
                 <p className="text-xl">{meal.strArea} </p>
                 <p className="text-xl">{meal.strCategory}</p>
